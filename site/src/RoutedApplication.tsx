73c2f5bfaf739fc6afbf5076d5a38f033a195e53
Navish Kumar <43814868+KumarNavish@users.noreply.github.com>
2026-08-28 17:25:29 +0200

    refactor(field): isolate routed application component

---
+++ site/src/RoutedApplication.tsx
@@ -0,0 +1,24 @@
+import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
+
+import App from './App.tsx'
+
+export function RoutedApplication() {
+  const location = useLocation()
+
+  useEffect(() => {
+    const queryChapter = new URLSearchParams(location.search).get('chapter')
+    const targetId = location.hash ? location.hash.slice(1) : queryChapter
+    if (!targetId) {
+      return undefined
+    }
+
+    const frame = window.requestAnimationFrame(() => {
+      document.getElementById(targetId)?.scrollIntoView({ block: 'start' })
+    })
+
+    return () => window.cancelAnimationFrame(frame)
+  }, [location.hash, location.search])
+
+  return <App />
+}
