21689cc4aa85faf9968ea89b39c553b9fec53fb5
Navish Kumar <43814868+KumarNavish@users.noreply.github.com>
2026-08-28 17:01:29 +0200

    fix(field): restore deep-linked chapter navigation

--- site/src/main.tsx
+++ site/src/main.tsx
@@ -1,14 +1,32 @@
-import { StrictMode } from 'react'
+import { StrictMode, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, useLocation } from 'react-router-dom'
 
 import './index.css'
 import App from './App.tsx'
 
+function RoutedApplication() {
+  const location = useLocation()
+
+  useEffect(() => {
+    const queryChapter = new URLSearchParams(location.search).get('chapter')
+    const targetId = location.hash ? location.hash.slice(1) : queryChapter
+    if (!targetId) {
+      return
+    }
+
+    window.setTimeout(() => {
+      document.getElementById(targetId)?.scrollIntoView({ block: 'start' })
+    }, 0)
+  }, [location.hash, location.search])
+
+  return <App />
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <RoutedApplication />
     </BrowserRouter>
   </StrictMode>,
 )
