52b0f1230c6b28e029b0acf49a587a4e49306d92
Navish Kumar <43814868+KumarNavish@users.noreply.github.com>
2026-08-28 17:25:40 +0200

    fix(field): keep entrypoint free of component declarations

--- site/src/main.tsx
+++ site/src/main.tsx
@@ -1,27 +1,9 @@
-import { StrictMode, useEffect } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, useLocation } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 import './index.css'
-import App from './App.tsx'
-
-function RoutedApplication() {
-  const location = useLocation()
-
-  useEffect(() => {
-    const queryChapter = new URLSearchParams(location.search).get('chapter')
-    const targetId = location.hash ? location.hash.slice(1) : queryChapter
-    if (!targetId) {
-      return
-    }
-
-    window.setTimeout(() => {
-      document.getElementById(targetId)?.scrollIntoView({ block: 'start' })
-    }, 0)
-  }, [location.hash, location.search])
-
-  return <App />
-}
+import { RoutedApplication } from './RoutedApplication.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
